7615554bb60a4f00b86177f69c0c67b96496de00
Jessé Oliveira <82299456+JesseLD@users.noreply.github.com>
2024-01-20 18:08:28 +0000

    configuração do `.editorconfig` e do `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return (<h1>Alana, Meu amor eu reservei esse pedacinho da internet com muito carinho só para você, Te Amo ❤</h1>);
-}
+  return (
+    <h1>
+      Alana, Meu amor eu reservei esse pedacinho da internet com muito carinho
+      só para você, Te Amo ❤
+    </h1>
+  );
+};
 
 export default Home;
